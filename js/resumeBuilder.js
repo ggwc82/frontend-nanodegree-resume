dd8b6f20e18a33cf5ac10565fb14b41e866f9e8e
Godfrey Chiu <godfrey.chiu@hssmi.org>
2017-04-12 16:17:20 +0100

    added jobs loop

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -110,3 +110,13 @@ if (bio.skills.length !== 0) {
 	formattedSkill = HTMLskills.replace("%data%", bio.skills[2]);
 	$("#skills").append(formattedSkill);	
 }
+
+for (job in work.jobs) {
+	$("#workExperience").append(HTMLworkStart);
+
+	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+	var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+	var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+	$(".work-entry:last").append(formattedEmployerTitle);
+}
